d7b3d6695d4d26818a293778a30220d55fb98c39
Anmolkumar2002 <anmolkumar200210@gmail.com>
2023-07-31 16:58:18 +0530

    intial commit

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Entet two number : ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "Sum : " << c;
+    return 0;
+}
